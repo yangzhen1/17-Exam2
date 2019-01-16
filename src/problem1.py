d13e5fdf2920357e022b181e1ad03739d24e5b88
yangz4 <yangz4@rose-hulman.edu>
2019-01-15 20:47:27 -0500

    zhen Yang

--- src/problem1.py
+++ src/problem1.py
@@ -3,8 +3,8 @@ Exam 2, problem 1.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zhen Yang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -12,14 +12,14 @@ import time
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    # run_test_problem1a()
-    # run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1a()
+    run_test_problem1b()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-strings for the  sum_of_digits  and  is_prime
+# DONE: 2.  READ the doc-strings for the  sum_of_digits  and  is_prime
 # functions defined below.  They are the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -131,6 +131,9 @@ def run_test_problem1a():
 
 
 def problem1a(sequence):
+    sum = sequence[0] + sequence[len(sequence)-1]
+    return sum
+
     """
     What comes in:  A sequence of numbers.  You may assume that the sequence
       contains at least 2 numbers.
@@ -145,7 +148,7 @@ def problem1a(sequence):
       -- problem1a( (30, 1, 22, 8, 5) )  returns 35.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
@@ -274,6 +277,13 @@ def run_test_problem1b():
 
 
 def problem1b(strings):
+    list = []
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])):
+            list += [strings[k]]
+    return list
+
+
     """
     What comes in:  A sequence of strings.  You may assume that each string
       has length at least 2.
@@ -293,7 +303,7 @@ def problem1b(strings):
            FOR HELP if this problem's specification is not clear to you.
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
 
@@ -369,6 +379,11 @@ def run_test_problem1c():
 
 
 def problem1c(integers):
+    for k in range(len(integers)):
+        if integers[k] < k:
+            return integers[k]
+    return 999
+
     """
     What comes in:  A sequence of integers.
     What goes out:
@@ -389,7 +404,7 @@ def problem1c(integers):
            their respective indices (0, 1, and 2, respectively).
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
 
@@ -505,6 +520,13 @@ def run_test_problem1d():
 
 
 def problem1d(t, sequence):
+    pdt = 1
+    for k in range(len(sequence) // 2, len(sequence)):
+        if sum_of_digits(sequence[k]) > t:
+            pdt = pdt * sequence[k]
+    return pdt
+
+
     """
     What comes in:  An integer t and a sequence of positive integers.
       You may assume that the length of the sequence is even.
@@ -523,7 +545,7 @@ def problem1d(t, sequence):
            FOR HELP if this problem's specification is not clear to you.
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
 
